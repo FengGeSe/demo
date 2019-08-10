8c7bb2cfbb529c5265bb1b332ab97d07caba120d
wushuaishuai <wushauishuai@zuoyebang.com>
2019-08-10 14:49:36 +0800

    删除不用的文件

--- transport/grpc/handler.go
+++
@@ -1,9 +0,0 @@
-package grpc
-
-import (
-	"context"
-
-	"google.golang.org/grpc"
-)
-
-type MethodHandler func(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error)
